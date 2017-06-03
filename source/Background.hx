54ac973acf385920674a2fbe1b2469810b2a89f0
Josh Schonstal <josh@schonstal.org>
2017-06-03 16:34:01 -0700

    ✨ Cycle through backgrounds

--- source/Background.hx
+++ source/Background.hx
@@ -7,6 +7,8 @@ import flixel.math.FlxPoint;
 
 class Background extends FlxSpriteGroup {
   var background:FlxSprite;
+  var backgrounds:Array<FlxSprite> = new Array<FlxSprite>();
+  var activeIndex:Int = 0;
 
   public function new():Void {
     super();
@@ -16,11 +18,29 @@ class Background extends FlxSpriteGroup {
     for (path in backgroundPaths) {
       background = new FlxSprite();
       background.loadGraphic(path);
+      backgrounds.push(background);
+      background.visible = false;
       add(background);
     }
+
+    backgrounds[0].visible = true;
   }
 
   public override function update(elapsed:Float):Void {
     super.update(elapsed);
+
+    if (FlxG.keys.justPressed.LEFT) {
+      activeIndex--;
+      if (activeIndex < 0) activeIndex = backgrounds.length - 1;
+    } else if (FlxG.keys.justPressed.RIGHT) {
+      activeIndex++;
+      if (activeIndex > backgrounds.length - 1) activeIndex = 0;
+    }
+
+    for (bg in backgrounds) {
+      bg.visible = false;
+    }
+
+    backgrounds[activeIndex].visible = true;
   }
 }
